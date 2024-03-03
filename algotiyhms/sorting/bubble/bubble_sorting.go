c46fb1d5f1c446f8a14af037cd88a020d0eff70c
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-03-03 11:06:28 -0300

    more comments

--- algotiyhms/sorting/bubble/bubble_sorting.go
+++ algotiyhms/sorting/bubble/bubble_sorting.go
@@ -17,3 +17,18 @@ func BubbleSorting(slice []int64, comparable enums.Comparable) []int64{
 
 	return slice
 }
+
+/* 
+1.	The outer loop represents the number of swaps that are done for the comparison of data.
+
+2.	The inner loop is used for the comparison of data. In the first iteration,
+	the largest value will be  moved to the end of the array, in the second iteration the
+	second-largest value will be moved  before the largest value and so on.
+
+3.	The greater() function is used for comparison, which means when the value of 
+	the first  argument is greater than the value of the second argument then performs a swap.
+	With this, we  are sorting in increasing order. If we have, the less() function in place
+	of greater() then the array  will be sorted in decreasing order. 
+
+	Jain, Hemant. Data Structures & Algorithms In Go (p. 49). Edição do Kindle. 
+*/
---
+++ algotiyhms/sorting/insert/resume.md
@@ -0,0 +1,23 @@
+## Insertion Sort
+
+* Simple sorting algorithm;
+* One item at a time by comparisions;
+* Like a deck of cards organization;
+
+![an example](https://upload.wikimedia.org/wikipedia/commons/0/0f/Insertion-sort-example-300px.gif)
+
+## Performance
+
+* Worst-case performance:
+  * O(n<sup>2</sup>) compariosons;
+  * O(n<sup>2</sup>) swaps;
+* Best-case performance:
+  * O(n) comparisons;
+  * O(1) swaps;
+* Average performance:
+  * O(n<sup>2</sup>) compariosons;
+  * O(n<sup>2</sup>) swaps;
+* Worst-case space complexity:
+  * O(n) total;
+  * O(1) auxiliary;
+
