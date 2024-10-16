557555b53f40d23222cf25d7469b5150525810cc
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-10-16 14:29:52 -0300

    feat: add selection sort

--- algotiyhms/sorting/selection/selection_sort.go
+++ algotiyhms/sorting/selection/selection_sort.go
@@ -1,19 +1,33 @@
 package sorting
 
-// SelectionSort is a func to sort an integer list by selection method
+// SelectionSortByMaxIndex is a func to sort an integer list by selection method with the max index
 func SelectionSortByMaxIndex(arr []int64) {
 	size := len(arr)
 	for i := 0; i < size; i++ {
 		maxIdx := 0
 		sizeI := size - 1 - i
 		for j := 1; j <= sizeI; j++ {
-			arrj := arr[j]
-			arrmaxIdx := arr[maxIdx]
-			if arrj > arrmaxIdx {
+			if arr[j] > arr[maxIdx] {
 				maxIdx = j
 			}
 		}
 		arr[sizeI], arr[maxIdx] = arr[maxIdx], arr[sizeI]
+	}
+
+}
+
+// SelectionSortByMinIndex is a func to sort an integer list by selection method with the max index
+func SelectionSortByMinIndex(arr []int64) {
+	size := len(arr)
+	for i := 0; i < size; i++ {
+		minIdx := 0
+		sizeI := size - 1 - i
+		for j := 1; j <= sizeI; j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		arr[sizeI], arr[minIdx] = arr[minIdx], arr[sizeI]
 	}
 
 }
