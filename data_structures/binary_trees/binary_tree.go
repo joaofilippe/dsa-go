32b12dc8520a6ab4963554b955ba44834a392741
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-10-20 10:59:21 -0300

    feat: changes root to be a type of leaf

--- data_structures/binary_trees/binary_tree.go
+++ data_structures/binary_trees/binary_tree.go
@@ -1,15 +1,30 @@
 package binarytrees
 
-type Root struct {
-	Value int
-	Left  *Leaf
-	Right *Leaf
-}
-
 type BinaryTree struct {
-	Root *Root
+	Root *Leaf
 }
 
 func NewBinaryTree(rootValue int) *BinaryTree {
-	return &BinaryTree{Root: &Root{Value: rootValue}}
+	return &BinaryTree{Root: &Leaf{Value: rootValue}}
+}
+
+func (b *BinaryTree) InsertLeaf(value int) {
+	leaf := NewLeaf(value)
+	if b.Root.Left == nil {
+		b.Root.Left = leaf
+	} else if b.Root.Right == nil {
+		b.Root.Right = leaf
+	} else {
+		insertLeafRecursive(b.Root)
+	}
+}
+
+func insertLeafRecursive(root *Leaf) {
+	if root.Left == nil {
+		root.Left = root
+	} else if root.Right == nil {
+		root.Right = root
+	} else {
+		insertLeafRecursive(root.Left)
+	}
 }
