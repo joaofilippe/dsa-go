315fad83cf080227d5d135bd47aafe21449d2799
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-10-23 20:13:14 -0300

    feat: add tests for height

--- data_structures/binary_trees/node_test.go
+++ data_structures/binary_trees/node_test.go
@@ -16,3 +16,33 @@ func TestInsertNode(t *testing.T) {
 		}
 	}
 }
+
+
+
+func TestHeight(t *testing.T) {
+	tests := []struct {
+		values   []int
+		expected int
+	}{
+		{[]int{}, 1},
+		{[]int{1}, 2},
+		{[]int{1, 2}, 3},
+		{[]int{1, 2, 3}, 4},
+		{[]int{3, 2, 1}, 4},
+		{[]int{1, 2, 3, 4}, 5},
+		{[]int{4, 3, 2, 1}, 5},
+		{[]int{1, 2, 3, 4, 5}, 6},
+	}
+
+	for _, test := range tests {
+		root := NewNode(0)
+		for _, value := range test.values {
+			root.Insert(value)
+		}
+
+		if height := root.Height(); height != test.expected {
+			t.Errorf("For values %v, expected height %d, got %d", test.values, test.expected, height)
+		}
+	}
+}
+
